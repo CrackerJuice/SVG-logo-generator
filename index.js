d4f4df87cb55f3226ba03be6e324435b2a679e48
CrackerJuice <Alex.marie3976@gmail.com>
2024-07-19 00:03:44 -0700

    Got it to generate the right shape instead of just squares.

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {circle, triangle, square}= require ("./lib/shapes");
+const { circle, triangle, square } = require('./lib/shapes');
 
-const questions =[
-    {
-        type: "checkbox",
-        name: "shapes",
-        message: "What shape do you want your logo to be?",
-        choices: ["circle", "triangle", "square"]
-    },
-    {
-        type:"input",
-        name:"bgColor",
-        message:"What color do you want the background of your logo to be? ",
-    },
-    {
-        type:"input",
-        name:"text",
-        message:"Enter up to three characters to be put on your logo. ",
-    },
-    {
-        type: "input",
-        name:"textColor",
-        message:"What color do you want the text on your logo to be? "
-    }
+const questions = [
+  {
+    type: 'list',//changes checkbox type to list type 
+    name: 'shape',
+    message: 'What shape do you want your logo to be?',
+    choices: ['circle', 'triangle', 'square'],
+  },
+  {
+    type: 'input',
+    name: 'bgColor',
+    message: 'What color do you want the background of your logo to be?',
+  },
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters to be put on the logo:',
+    validate: (input) => input.length <= 3 || 'Text should be 3 characters or less.',
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'What color do you want the text on your logo to be?',
+  },
 ];
 
-function generateSVG(shapes, bgColor, text, textColor) {
+function generateSVG(shape, bgColor, text, textColor) {
+  let shapePath = '';
+
+  switch (shape) {
+    case 'circle':
+      shapePath = circle();
+      break;
+    case 'triangle':
+      shapePath = triangle();
+      break;
+    case 'square':
+      shapePath = square();
+      break;
+    default:
+      break;
+  }
+
   return `
     <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500">
-      <rect width="100%" height="100%" fill="${bgColor}" />
+      <rect width="100%" height="100%" fill="white" />
+      <path d="${shapePath}" fill="${bgColor}" />
       <text x="50%" y="50%" text-anchor="middle" fill="${textColor}" font-size="48" dy=".3em">
         ${text}
       </text>
@@ -40,9 +58,9 @@ function generateSVG(shapes, bgColor, text, textColor) {
 async function writeToFile(fileName, data) {
   try {
     await fs.promises.writeFile(fileName, data);
-    console.log("Logo successfully generated!");
+    console.log('Logo successfully generated!');
   } catch (error) {
-    console.error("Could not generate", error);
+    console.error('Could not generate logo:', error);
   }
 }
 
@@ -50,7 +68,7 @@ async function main() {
   console.log('Starting logo generator...');
 
   const answers = await inquirer.prompt(questions);
-  const svg = generateSVG(answers.shapes, answers.bgColor, answers.text, answers.textColor);
+  const svg = generateSVG(answers.shape, answers.bgColor, answers.text, answers.textColor);
 
   await writeToFile('logo.svg', svg);
 }
